69aa481fc6b87d03799b812f54b323e90d100c1f
Patryk Zdunowski <zdunekhere@gmail.com>
2025-03-20 23:56:35 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go.livesession.io/hello-govarnityurl
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"go.livesession.io/hello-govarnityurl/lib"
+)
+
+func main() {
+	fmt.Println(lib.Hello())
+}
